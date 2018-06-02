747a201db0bf716a91d73de6ffb1c0be168a4d9a
Kyle Verret <kyleverret@yahoo.com>
2018-06-02 15:33:08 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -30,7 +30,7 @@ function pressIt(){
 }
 
 function submitIt(){
-  $('form input:last-child').on('submit', function(){
+  $('form').on('submit', function(){
     alert("Your form is going to be submitted now.");
   })
 }
